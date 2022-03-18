fa1499643a76890c53abdfd28e1c42a1d6c09f61
Chetan Saini <64543470+thespiritfull@users.noreply.github.com>
2022-03-18 18:11:59 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,35 +8,36 @@ const AppCache = [
     "css/index.css",
     "js/clock.js",
     "js/index.js",
-    "_pwa/favicon.svg",
-    "https://cdn.com/google/google_jquery.min.js",
-    "https://vectoricons.com/typefaces/css/?display=swap&family=Euclid+Square",
-    "https://cdn.com/google/google-material-icons/icon.css",
-    "https://cdn.com/google/google-material-icons/fonts/MaterialIcons-Regular.woff2",
-    "https://vectoricons.com/typefaces/e/Euclid%20Square/woff/EuclidSquare-Regular.woff"
+    "pwa/favicon.svg",
+    "https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js",
+    "https://vectoricons.000webhostapp.com/typefaces/css/?display=swap&family=Euclid+Square:w@400",
+    "https://fonts.googleapis.com/icon?family=Material+Icons|Material+Icons+Outlined",
+    "https://fonts.gstatic.com/s/materialicons/v126/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2",
+    "https://fonts.gstatic.com/s/materialiconsoutlined/v101/gok-H7zzDkdnRel8-DQ6KAXJ69wP1tGnf4ZGhUcel5euIg.woff2",
+    "https://vectoricons.000webhostapp.com/typefaces/e/Euclid%20Square/woff/EuclidSquare-Regular.woff"
 ];
 
 self.addEventListener("install", (e)=>{
 
-//     e.waitUntil(
-//         caches.open(AppCacheName).then(cache=>{
-//             cache.addAll(AppCache);
-//         })
-//     );
+    e.waitUntil(
+        caches.open(AppCacheName).then(cache=>{
+            cache.addAll(AppCache);
+        })
+    );
 
 });
 
 self.addEventListener("fetch", (e)=>{
 
-//     e.respondWith(
-//         caches.match(e.request).then(resCache=>{
-//             return resCache || fetch(e.request).then(res=>{
-//                 caches.open(AppCacheName).then(cache=>{
-//                     cache.put(e.request, res.clone());
-//                     return res;
-//                 });
-//             })
-//         })
-//     );
+    e.respondWith(
+        caches.match(e.request).then(resCache=>{
+            return resCache || fetch(e.request).then(res=>{
+                caches.open(AppCacheName).then(cache=>{
+                    cache.put(e.request, res.clone());
+                    return res;
+                });
+            })
+        })
+    );
 
 });
