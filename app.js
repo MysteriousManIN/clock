b9580165cee38a792e31a2bf1ee9475d8f64d8f0
Chetan Saini <64543470+MysteriousManIN@users.noreply.github.com>
2022-07-26 10:52:16 +0530

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-if("serviceWorker" in window.navigator){
-    window.navigator.serviceWorker.register("sw.js").then(
-        (res) =>{ /*console.log(res.scope);*/ },
-        (err) =>{ console.log(err); }
-    );
-}
